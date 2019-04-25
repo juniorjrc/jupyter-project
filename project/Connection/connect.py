21db5b41059266e0b91a6680d1a6aacfd948749e
juniorjrc <juniorjrcsccp@gmail.com>
2019-04-25 11:15:02 -0300

    Alterando as configurações de conexão

--- project/Connection/connect.py
+++ project/Connection/connect.py
@@ -1,14 +1,19 @@
-import pymysql
+try:
+    import pymysql
+except:
+    import subprocess
+    subprocess.call(["pip", "install", "pymysql"])
+    import pymysql
 
 #INSTÂNCIA DA CONEXÃO COM O BANCO
 class Conexao():
     def __init__(self):
         self.conexao = pymysql.connect(
-            host='localhost',
+            host='172.17.0.1',
             port=3307,
             database='PROJREDES',
-            user='root',
-            password='root'
+            user='MainUser',
+            password='MainPassword'
         )
 
         self.bd = self.conexao.cursor()
