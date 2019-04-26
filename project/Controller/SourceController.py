74b75cf4d006aa7709445d83fa186dc8d86cae28
juniorjrc <juniorjrcsccp@gmail.com>
2019-04-26 10:05:04 -0300

    Removendo arquivos desnecessários

--- project/Controller/SourceController.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from matplotlib import pylab, mlab, pyplot
-import seaborn as sns
-plt = pyplot
-plt.style.use('ggplot')
-
-class SourceController():
-    def readCSV(self, file):
-        return pd.read_csv(file, sep=';', encoding='utf-8')
